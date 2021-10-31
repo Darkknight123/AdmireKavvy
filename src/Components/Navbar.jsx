e9ddb5925a4d16682823b47f3e43e5444cb1c9e1
Darkknight123 <philipkerry57@gmail.com>
2021-10-31 12:54:08 +0300

    finished working on the navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,18 +1,39 @@
-import React from 'react'
-import styled, { isStyledComponent } from 'styled-components'
-
-const Container = styled.div`
- height: 60px;
- Background-color:;
-
-`
+import { IconButton } from "@material-ui/core";
+import { Search } from "@material-ui/icons";
+import React from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
-    return (
-        <div>
-          Navbar  
-        </div>
-    )
-}
+  return (
+    <div className="navbar-container">
+      <div className="wrapper">
+        <div className="left">
+          <div className="language">En</div>
+          <div className="search-container">
+            <div className="input"></div>
 
-export default Navbar
+            <Search />
+          </div>
+        </div>
+        <div className="Center">
+          <div className=" logo">
+            <h1>AdmireKavvy</h1>
+          </div>
+        </div>
+        <div className="Right">
+          <div className=" menu-item">Register</div>
+          <div className=" menu-item">Login</div>
+          <div className=" menu-item">
+            <IconButton ariel-label="cart">
+              <StyledBadge badgeContent={0} color="pimary">
+              <ShoppingCartIcon />
+              </StyledBadge>
+            </IconButton>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Navbar from '../Components/Navbar'
 
 
-
 export default function Home() {
     return (
         <div>
