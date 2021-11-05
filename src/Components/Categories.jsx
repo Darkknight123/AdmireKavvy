c8d21c4f78b8da8525ab5bd81b146653e81d2637
Darkknight123 <philipkerry57@gmail.com>
2021-11-05 20:52:27 +0300

    working on the categories section

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -12,25 +12,28 @@ function Categories() {
         <div className="categories">
           <img src={bestfriends} />
           <div className="info">
-              <h2>Couples Jewelery</h2>
+              <h2>Couples/Bestfriend Jewelery</h2>
+        
           </div>
-          <button> Shop </button>
+          
           
         </div>
         <div className="categories">
           <img src={necklaces} />
           <div className="info">
-              <h2>Couples Jewelery</h2>
+              <h2>Necklaces</h2>
+            
           </div>
-          <button> Shop</button>
+          
           
         </div>
         <div className="categories">
           <img src={rings} />
           <div className="info">
-              <h2>Couples Jewelery</h2>
+              <h2>Rings And Bracelets</h2>
+              
           </div>
-          <button> Shop</button>
+          
           
         </div>
        
