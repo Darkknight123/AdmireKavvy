5c541adb14023513c565675ae330f0f79dbcc5c0
Darkknight123 <philipkerry57@gmail.com>
2021-11-19 12:29:21 +0300

    Done with the footer and contact section

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,46 +1,49 @@
 import { Facebook, Instagram, MusicNote, Twitter, WhatsApp } from '@material-ui/icons'
 import React from 'react'
+import "./Footer.css"
+import logo from "../images/bfcouples/logo.jpg"
 
 function Footer() {
     return (
-      <div className="container">
-        <div className="left">
-          <div className="Logo"></div>
-          <div className="description"></div>
-          <div className="socialContainer">
-            <div className="socialIcons">
-              <Facebook />
-            </div>
-            <div className="socialIcons">
-              <Instagram />
-            </div>
-            <div className="socialIcons">
-              <Twitter />
-            </div>
-            <div className="socialIcons">
-              <WhatsApp />
-            </div>
-            <div className="socialIcons">
-              <MusicNote/>
-            </div>
-          </div>
+      <div className="Footercontainer">
+        <div className="leftFooter">
+        <img src={logo} alt="logo" className="logoimg"/>
         </div>
-        <div className="Right">
-            <h2> About AdmireKavvy</h2>
-            <aside>
+        <div className="centerFooter">
+          <h2 className="title">Useful Links</h2>
+          <ul className="useful">
+            <ul>Home</ul>
+            <ul>Cart</ul>
+            <ul>My Account</ul>
+            <ul>Wishlist</ul>
+            <ul>Terms and Conditions</ul>
+          </ul>
+        </div>
+        <div className="rightFooter">
+            <h2> About Us</h2>
             <article>
                 We an upcoming jewellery shop offering our esteemed customers
                 reliable, affordable and high quality products.Feel free to 
                 browse around and contact us if you need any help
-
             </article>
-
-            </aside>
-        
+            <div className="socialContainer">
+            <div className="socialIcons" >
+              <Facebook style={{color:"#3b5998"}}/>
+            </div>
+            <div className="socialIcons">
+              <Instagram style={{color:"#E4405F"}}/>
+            </div>
+            <div className="socialIcons">
+              <Twitter style={{color:"00acee"}}/>
+            </div>
+            <div className="socialIcons">
+              <WhatsApp style={{color:"#075e54"}}/>
+            </div>
+            <div className="socialIcons">
+              <MusicNote style={{color:"#ff0050"}}/>
+            </div>
+          </div>
         </div>
-
-        <div className="center"></div>
-        <div className="Right"></div>
       </div>
     );
 }
