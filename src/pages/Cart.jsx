222ce31522f370e7b7d73e6dc9e86c5d36848ca6
Darkknight123 <philipkerry57@gmail.com>
2021-11-23 23:04:48 +0300

    done wih the cart page design

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,3 +1,4 @@
+import { Add, Remove } from '@material-ui/icons'
 import React from 'react'
 import Footer from '../Components/Footer'
 import Navbar from '../Components/Navbar'
@@ -28,10 +29,54 @@ function Cart() {
                                     <span className="productsize"><b>Size:</b> 16</span>
                                 </div>
                             </div>
-                            <div className="pricedetails"></div>
+                            <div className="pricedetails">
+                                <div className="amountcontainer">
+                                    <Add/>
+                                    <div className="productamount">1</div>
+                                    <Remove/>
+                                </div>
+                                <div className="Productprice">Ksh.500</div>
+                            </div>
+                        </div>  
+                        <hr/>
+                        <div className="productss">
+                            <div className="productdetails">
+                                <img src="https://www.pexels.com/photo/adult-affection-beads-blur-371285/" className="cartimg" alt="cartimage"/>
+                                <div className="detailed">
+                                    <span className="productname"><b>Product:</b> friendship bracelets</span>
+                                    <span className="productid"><b>ID:</b> e3648f9362</span>
+                                    <span className="productsize"><b>Size:</b> 16</span>
+                                </div>
+                            </div>
+                            <div className="pricedetails">
+                                <div className="amountcontainer">
+                                    <Add/>
+                                    <div className="productamount">1</div>
+                                    <Remove/>
+                                </div>
+                                <div className="Productprice">Ksh.500</div>
+                            </div>
                         </div>  
                     </div>
-                    <div className="summary"></div>
+                    <div className="summary">
+                        <h2 className="summarytitle">
+                            Order Summary
+                        </h2>
+                        <div className="summaryItem">
+                            <span className="itemtext">Subtotal</span>
+                            <span className="itemprice">Ksh.1000</span>
+                        </div>
+                        <div className="summaryItem">
+                            <span className="itemtext">Delivery Fee</span>
+                            <span className="itemprice">Ksh.100</span>
+                        </div>
+                        <div className="summaryItem">
+                            <span className="itemtotal" type="total">Total</span>
+                            <span className="totalprice">Ksh.1100</span>
+                        </div>
+                        <button className="summarybutton">Checkout Now</button>
+
+                    </div>
                 </div>
             </div>
             <Footer/>
