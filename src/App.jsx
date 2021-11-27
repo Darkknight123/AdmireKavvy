74bb03caa1f9bdeddfb46c2dcbfc1ebbeb2bf463
Darkknight123 <philipkerry57@gmail.com>
2021-11-27 10:34:12 +0300

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,16 @@ import React from 'react'
 import ProductList from "./pages/ProductList";
 import ProductView from "./pages/ProductView";
 import Cart from "./pages/Cart";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
-   return <div><Cart/></div>
+   return <Router>
+      <div><Home/>
+      <ProductList/>
+      <ProductView/>
+      <Cart/>
+      </div>
+   </Router>
 }
 
 export default App;
